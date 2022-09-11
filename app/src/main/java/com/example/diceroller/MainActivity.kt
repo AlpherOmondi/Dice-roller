90a23e95a826b519a72b7cc6045c23e9c536410a
AlpherDev <alphaomondi001@gmail.com>
2022-09-11 09:16:00 +0300

    feat:Added a toast

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var myButton: Button = findViewById(R.id.roll_button)
         myButton.text="Let's Roll"
+        myButton.setOnClickListener{
+           Toast.makeText(this,"We are rolling", LENGTH_SHORT)
+        }
 
     }
 }
