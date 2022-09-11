e961702c6e84ffa565e3b2774a82e347b30492c6
AlpherDev <alphaomondi001@gmail.com>
2022-09-11 12:50:55 +0300

    feat:Added random integer generation

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import java.util.*
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +16,16 @@ class MainActivity : AppCompatActivity() {
         var myButton: Button = findViewById(R.id.roll_button)
         myButton.text="Let's Roll"
         myButton.setOnClickListener{
-           Toast.makeText(this,"We are rolling", LENGTH_SHORT)
+           //Toast.makeText(this,"We are rolling", LENGTH_SHORT)
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        var myText :TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt()+1
+        myText.text=randomInt.toString()
     }
 }
